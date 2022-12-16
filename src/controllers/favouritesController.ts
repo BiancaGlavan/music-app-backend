6e45df901d7818a790da274e6d06cba6cabea19b
BiancaGlavan <bianca.glavan@yahoo.ro>
2022-12-16 23:51:50 +0200

    update prettier and eslint settings

--- src/controllers/favouritesController.ts
+++ src/controllers/favouritesController.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from "express";
-import Artist from "../models/Artist";
-import User from "../models/User";
+import { Request, Response, NextFunction } from 'express';
+import Artist from '../models/Artist';
+import User from '../models/User';
 
 export const getAlbums = async (req: Request, res: Response, next: NextFunction) => {};
 
@@ -10,7 +10,7 @@ export const getArtists = async (req: Request, res: Response, next: NextFunction
   const userId = req.userId;
 
   try {
-    const user = await User.findById(userId).populate("artists");
+    const user = await User.findById(userId).populate('artists');
     return res.status(200).json(user?.artists);
   } catch (error) {
     return res.status(400).json(error);
@@ -26,7 +26,7 @@ export const addToFavArtist = async (req: Request, res: Response, next: NextFunc
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(400).json("Something went wrong");
+      return res.status(400).json('Something went wrong');
     }
 
     // Check if artist exists in db
@@ -45,25 +45,28 @@ export const addToFavArtist = async (req: Request, res: Response, next: NextFunc
       return res.status(200).json({ artist: artist, user });
     }
   } catch (error) {
-    return res.status(400).json("Something went wrong!!!");
+    return res.status(400).json('Something went wrong!!!');
   }
 };
 
-export const removeFavArtist = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
-    const userId = req.userId;
-    const deezer_id = req.params.id;
-  
-    try {
-      const user = await User.findById(userId);
-      const artist = await Artist.findOne({ deezer_id: deezer_id });
-      if (!user || !artist) {
-        return res.status(400).json("Something went wrong");
-      }
-      
-      await user.updateOne({ $pull: { artists: artist.id } });
-      return res.status(200).json("Artist removed from favourites!");
-   
-    } catch (error) {
-      return res.status(400).json("Something went wrong!!!");
+export const removeFavArtist = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const userId = req.userId;
+  const deezer_id = req.params.id;
+
+  try {
+    const user = await User.findById(userId);
+    const artist = await Artist.findOne({ deezer_id: deezer_id });
+    if (!user || !artist) {
+      return res.status(400).json('Something went wrong');
     }
-  };
+
+    await user.updateOne({ $pull: { artists: artist.id } });
+    return res.status(200).json('Artist removed from favourites!');
+  } catch (error) {
+    return res.status(400).json('Something went wrong!!!');
+  }
+};
